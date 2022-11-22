b56656a04a83a9d98db07f1e789a12822b950a32
BaguetteYeeter <baguetteyeeter@gmail.com>
2022-11-22 10:51:27 +0000

    added more arithmetic opcodes

--- components/opcodes.py
+++ components/opcodes.py
@@ -19,4 +19,28 @@ def sub(bios: bios.BIOS) -> None:
     b = ord(bios.ram.fetch(address, 1))
     bios.cpu.Registers.A = a - b
 
-codes = {0x00: nop, 0x01: add, 0x02: sub}
+def mul(bios: bios.BIOS) -> None:
+    a = bios.cpu.Registers.A
+    address = bios.ram.fetch(bios.cpu.Registers.CurrentInstruction, 2)
+    bios.cpu.Registers.CurrentInstruction += 2
+    address = ord(address[0]) + (ord(address[1]) * 0x100)
+    b = ord(bios.ram.fetch(address, 1))
+    bios.cpu.Registers.A = a * b
+
+def div(bios: bios.BIOS) -> None:
+    a = bios.cpu.Registers.A
+    address = bios.ram.fetch(bios.cpu.Registers.CurrentInstruction, 2)
+    bios.cpu.Registers.CurrentInstruction += 2
+    address = ord(address[0]) + (ord(address[1]) * 0x100)
+    b = ord(bios.ram.fetch(address, 1))
+    bios.cpu.Registers.A = int(a / b)
+
+def mod(bios: bios.BIOS) -> None:
+    a = bios.cpu.Registers.A
+    address = bios.ram.fetch(bios.cpu.Registers.CurrentInstruction, 2)
+    bios.cpu.Registers.CurrentInstruction += 2
+    address = ord(address[0]) + (ord(address[1]) * 0x100)
+    b = ord(bios.ram.fetch(address, 1))
+    bios.cpu.Registers.A = int(a % b)
+
+codes = {0x00: nop, 0x01: add, 0x02: sub, 0x03: mul, 0x04: div, 0x05: mod}
--- components/storage.py
+++ components/storage.py
@@ -3,6 +3,6 @@ class FloppyController:
         self.disks = []
     class Disk:
         def __init__(self, file: str) -> None:
-            f = open(file, "r")
+            f = open(file, "r", encoding="latin-1")
             self.data = f.read()
             f.close()
